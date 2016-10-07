f3a97008626cae8c4a99db6834d8420bae1b8fda
LarryMckuydee <larry.mckuydee@gmail.com>
2016-10-07 12:20:44 +0800

    Create repo

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'json'
+get '/' do
+  file = File.read(File.expand_path('../data.json', __FILE__))
+  @data = JSON::parse(file)
+  @percentage = (@data["current"].to_f / @data["target"].to_f) * 100
+  erb :index
+end
+
+get '/edit' do
+  file = File.read(File.expand_path('../data.json', __FILE__))
+  @data = JSON::parse(file)
+  erb :edit
+end
+
+post '/update' do
+  File.write(File.expand_path('../data.json', __FILE__), params.to_json)
+  redirect "/"
+end
---
+++ config.ru
@@ -0,0 +1,5 @@
+Encoding.default_external = Encoding::UTF_8
+require File.expand_path '../app.rb', __FILE__
+
+
+run Sinatra::Application
